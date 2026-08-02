6beaf5fd6ba7d62aa41f296cfeb4efb41699a3bb
tnhanbernie <ttnhanbernie@gmail.com>
2026-08-02 16:35:57 +0700

    feat: logger

--- src/main/java/com/thuyen/bakeryshop/common/logging/RequestLoggingFilter.java
+++ src/main/java/com/thuyen/bakeryshop/common/logging/RequestLoggingFilter.java
@@ -1,4 +1,36 @@
 package com.thuyen.bakeryshop.common.logging;
 
-public class RequestLoggingFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        long startTime = System.currentTimeMillis();
+
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            long duration = System.currentTimeMillis() - startTime;
+
+            log.info(
+                    "{} {} ip={} status={} duration={}ms",
+                    request.getMethod(),
+                    request.getRequestURI(),
+                    request.getLocalAddr(),
+                    response.getStatus(),
+                    duration
+            );
+        }
+    }
 }
